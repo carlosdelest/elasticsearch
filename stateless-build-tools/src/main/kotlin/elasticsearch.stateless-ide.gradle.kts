b3d7c2cff4caf3b654cc503bae3b0e79c3a0ce60
Mark Vieira <portugee@gmail.com>
2023-01-05 11:57:36 -0800

    Add "confidential" license header to internal code (#46)
    This makes licensing here a bit more explicit. I borrowed this notice
    from another internal project. This also eliminates the false positive
    complaints from Checkstyle about duplicate header comments.

--- stateless-build-tools/src/main/kotlin/elasticsearch.stateless-ide.gradle.kts
+++ stateless-build-tools/src/main/kotlin/elasticsearch.stateless-ide.gradle.kts
@@ -7,7 +7,27 @@ plugins.withType<IdeaExtPlugin> {
     val settings = ideaProject.extensions.getByName("settings") as ExtensionAware
     val copyright = settings.extensions.getByType<CopyrightConfiguration>()
 
+    copyright.profiles.create("confidential") {
+        keyword = "ELASTICSEARCH CONFIDENTIAL"
+        notice = """
+            ELASTICSEARCH CONFIDENTIAL
+            __________________
+            
+            Copyright Elasticsearch B.V. All rights reserved.
+            
+            NOTICE:  All information contained herein is, and remains
+            the property of Elasticsearch B.V. and its suppliers, if any.
+            The intellectual and technical concepts contained herein
+            are proprietary to Elasticsearch B.V. and its suppliers and
+            may be covered by U.S. and Foreign Patents, patents in
+            process, and are protected by trade secret or copyright
+            law.  Dissemination of this information or reproduction of
+            this material is strictly forbidden unless prior written
+            permission is obtained from Elasticsearch B.V.
+        """.trimIndent()
+    }
+
     // We only want to use the "default" Elastic+SSPL license header for open source code
-    copyright.useDefault = ""
+    copyright.useDefault = "confidential"
     copyright.scopes = mapOf("x-pack" to "Elastic", "llrc" to "Apache2", "elasticsearch" to "Default")
 }
