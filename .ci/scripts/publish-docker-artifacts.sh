f90c9b21b21b91e5495146923f2a337e82b8c71e
Mark Vieira <portugee@gmail.com>
2023-02-09 14:03:41 -0800

    Publish docker images from buildkite CI pipeline (#128)

---
+++ .ci/scripts/publish-docker-artifacts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+scripts_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+source $scripts_dir/run-gradle-buildkite.sh buildDockerImage buildAarch64DockerImage
+
+set +x
+export DOCKER_REGISTRY_USERNAME="$(vault read -field=username secret/ci/elastic-elasticsearch-serverless/prod_docker_registry_credentials)"
+export DOCKER_REGISTRY_PASSWORD="$(vault read -field=password secret/ci/elastic-elasticsearch-serverless/prod_docker_registry_credentials)"
+export DOCKER_IMAGE=docker.elastic.co/elasticsearch-ci/elasticsearch-serverless
+export X86_IMAGE_TAG=${DOCKER_IMAGE}:${BUILDKITE_COMMIT:0:7}-x86_64
+export ARM_IMAGE_TAG=${DOCKER_IMAGE}:${BUILDKITE_COMMIT:0:7}-aarch64
+docker tag elasticsearch-serverless:x86_64 ${X86_IMAGE_TAG}
+docker tag elasticsearch-serverless:aarch64 ${ARM_IMAGE_TAG}
+echo $DOCKER_REGISTRY_PASSWORD | docker login -u $DOCKER_REGISTRY_USERNAME --password-stdin docker.elastic.co
+unset DOCKER_REGISTRY_USERNAME DOCKER_REGISTRY_PASSWORD
+set -x
+docker push ${X86_IMAGE_TAG}
+docker push ${ARM_IMAGE_TAG}
+docker manifest create ${DOCKER_IMAGE}:${BUILDKITE_COMMIT:0:7} --amend ${X86_IMAGE_TAG} --amend ${ARM_IMAGE_TAG}
+docker manifest create ${DOCKER_IMAGE}:latest --amend ${X86_IMAGE_TAG} --amend ${ARM_IMAGE_TAG}
+docker manifest push ${DOCKER_IMAGE}:${BUILDKITE_COMMIT:0:7}
+docker manifest push ${DOCKER_IMAGE}:latest
