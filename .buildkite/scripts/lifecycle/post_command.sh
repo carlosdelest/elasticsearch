aed22f778ada6345d7156dbb7ecf7e7f4fc330b8
Mark Vieira <portugee@gmail.com>
2023-03-07 11:19:42 -0800

    Update CI pipeline to support updating elasticsearch submodule

--- .buildkite/scripts/lifecycle/post_command.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "BUILDKITE_ARTIFACT_UPLOAD_DESTINATION: $BUILDKITE_ARTIFACT_UPLOAD_DESTINATION"
-find . -type d -regex ".*build/reports" -print0 | tar -cvjf build-reports.tar.bz2 --null --files-from -
-buildkite-agent artifact upload build-reports.tar.bz2
---
+++ .ci/scripts/update-es-submodule.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Updating elasticsearch submodule to $(git -C elasticsearch rev-parse HEAD)"
+git config user.name "elasticsearchmachine"
+git config user.email "infra-root+elasticsearchmachine@elastic.co"
+git add elasticsearch
+git commit -m "Update elasticsearch submodule"
+git push origin HEAD:${BUILDKITE_BRANCH}
