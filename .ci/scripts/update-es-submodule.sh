addae22c003ee0e08654e49ab15a5ad55ff7d25d
Mark Vieira <portugee@gmail.com>
2023-03-08 08:36:15 -0800

    Update unix file permissions on CI scripts

--- .ci/scripts/update-es-submodule.sh
+++ .ci/scripts/update-es-submodule.sh
@@ -6,5 +6,5 @@ git config user.name "elasticsearchmachine"
 git config user.email "infra-root+elasticsearchmachine@elastic.co"
 git add elasticsearch
 git commit -m "Update elasticsearch submodule"
-git pull --rebase
+git pull --rebase origin ${BUILDKITE_BRANCH}
 git push origin HEAD:${BUILDKITE_BRANCH}
