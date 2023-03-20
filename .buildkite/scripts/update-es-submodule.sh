02e9ffb06e83f96bd68c28da33d5d90b271a5a88
David Turner <david.turner@elastic.co>
2023-03-20 16:31:16 +0000

    Skip empty submodule updates (#201)

--- .buildkite/scripts/update-es-submodule.sh
+++ .buildkite/scripts/update-es-submodule.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e
+set -exuo pipefail
 
 echo "Updating elasticsearch submodule to $(git -C elasticsearch rev-parse HEAD)"
 git config user.name "elasticsearchmachine"
 git config user.email "infra-root+elasticsearchmachine@elastic.co"
 git add elasticsearch
-git commit -m "Update elasticsearch submodule"
-git pull --rebase origin ${BUILDKITE_BRANCH}
-git push origin HEAD:${BUILDKITE_BRANCH}
+if ! git diff-index --quiet --cached HEAD
+then
+  git commit -m "Update elasticsearch submodule"
+  git pull --rebase origin ${BUILDKITE_BRANCH}
+  git push origin HEAD:${BUILDKITE_BRANCH}
+fi
