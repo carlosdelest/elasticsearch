625405d69cbb3902fc4514c9aff6f89068d805d0
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2023-05-03 10:12:53 -0700

    Fix promote commit script

--- .buildkite/scripts/tag-promoted-commit.sh
+++ .buildkite/scripts/tag-promoted-commit.sh
@@ -11,6 +11,7 @@ git push -f origin current_dev current_qa
 
 echo "--- Tag and push 'latest' image manifest"
 export GIT_ABBREV_COMMIT=git-${BUILDKITE_COMMIT:0:12}
+export DOCKER_IMAGE=docker.elastic.co/elasticsearch-ci/elasticsearch-serverless
 export X86_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-x86_64
 export ARM_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-aarch64
 docker manifest create ${DOCKER_IMAGE}:latest --amend ${X86_IMAGE_TAG} --amend ${ARM_IMAGE_TAG}
