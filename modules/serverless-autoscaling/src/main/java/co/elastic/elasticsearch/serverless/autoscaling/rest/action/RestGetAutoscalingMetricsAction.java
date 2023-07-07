e2a62bd8bd9014bbb81c8f3f153108cf9295d0eb
Pooya Salehi <pxsalehi@users.noreply.github.com>
2023-07-07 09:29:03 +0200

    Add scope to serverless autoscaling API (#515)

--- modules/serverless-autoscaling/src/main/java/co/elastic/elasticsearch/serverless/autoscaling/rest/action/RestGetAutoscalingMetricsAction.java
+++ modules/serverless-autoscaling/src/main/java/co/elastic/elasticsearch/serverless/autoscaling/rest/action/RestGetAutoscalingMetricsAction.java
@@ -24,6 +24,8 @@ import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.Scope;
+import org.elasticsearch.rest.ServerlessScope;
 import org.elasticsearch.rest.action.RestCancellableNodeClient;
 import org.elasticsearch.rest.action.RestToXContentListener;
 
@@ -32,6 +34,7 @@ import java.util.List;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 
+@ServerlessScope(Scope.INTERNAL)
 public class RestGetAutoscalingMetricsAction extends BaseRestHandler {
 
     public static final String TIMEOUT = "timeout";
