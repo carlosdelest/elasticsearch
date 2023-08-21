6b5739b544ceb1c5f0db408874a9941262c6a2bf
Rene Groeschke <rene@elastic.co>
2023-08-21 16:58:49 +0200

    Update service name in proxy setup in dev deployment (#752)
    That service was renamed and our scripts need to be updated to take that into account

--- .buildkite/scripts/setup-proxies.sh
+++ .buildkite/scripts/setup-proxies.sh
@@ -24,4 +24,4 @@ aws_get_cluster_credentials
 
 echo '--- Expose global ingress nginx controller'
 
-kubectl expose svc/global-ingress-ingress-nginx-controller --port=8443 --target-port=443 --name=project-api-dev-proxy --type=LoadBalancer -n elastic-system
+kubectl expose svc/global-ingress-nginx-user-ingress-controller --port=8443 --target-port=443 --name=project-api-dev-proxy --type=LoadBalancer -n elastic-system
