2b2cd4c6a18c549efd64fd6ae24b29cebd6b1f4f
Mark Vieira <portugee@gmail.com>
2023-08-28 12:54:01 -0700

    Add nightly job for release promotion

--- .buildkite/scripts/tag-promoted-commit.sh
+++ .buildkite/scripts/tag-promoted-commit.sh
@@ -6,16 +6,18 @@ scripts_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source $scripts_dir/utils/docker.sh
 
 echo "--- Tag git commit"
+TAG_NAME=current_${GITOPS_ENV}
 echo "Tagging commit ${BUILDKITE_COMMIT}"
 git config user.name "elasticsearchmachine"
 git config user.email "infra-root+elasticsearchmachine@elastic.co"
-git tag -f current_dev ${BUILDKITE_COMMIT}
-git tag -f current_qa ${BUILDKITE_COMMIT}
-git push -f origin current_dev current_qa
+git tag -f TAG_NAME ${BUILDKITE_COMMIT}
+git push -f origin TAG_NAME
 
-echo "--- Tag and push 'latest' image manifest"
-GIT_ABBREV_COMMIT="${DEPLOY_ID:-git-${BUILDKITE_COMMIT:0:12}}"
-DOCKER_IMAGE=docker.elastic.co/elasticsearch-ci/elasticsearch-serverless
-X86_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-x86_64
-ARM_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-aarch64
-push_docker_manifest ${DOCKER_IMAGE}:latest ${X86_IMAGE_TAG} ${ARM_IMAGE_TAG}
+if [ "${GITOPS_ENV}" == "dev" ]; then
+  echo "--- Tag and push 'latest' image manifest"
+  GIT_ABBREV_COMMIT="${DEPLOY_ID:-git-${BUILDKITE_COMMIT:0:12}}"
+  DOCKER_IMAGE=docker.elastic.co/elasticsearch-ci/elasticsearch-serverless
+  X86_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-x86_64
+  ARM_IMAGE_TAG=${DOCKER_IMAGE}:${GIT_ABBREV_COMMIT}-aarch64
+  push_docker_manifest ${DOCKER_IMAGE}:latest ${X86_IMAGE_TAG} ${ARM_IMAGE_TAG}
+fi
