3062b73105c7d09021ab5a4f2679bcaedea11ca7
Rene Groeschke <rene@elastic.co>
2023-11-01 16:25:15 +0100

    Run Kibana serverless verify on merge intake (#1101)
    * Resolve latest succesful kibana e2e from buildkite
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

---
+++ .buildkite/scripts/run-e2e-kibana-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+scripts_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source $scripts_dir/utils/misc.sh
+
+echo "--- Determining last succesful kibana e2e tests"
+KIBANA_BRANCH='main'
+INTAKE_PIPELINE_SLUG="kibana-elasticsearch-serverless-verify-and-promote"
+BUILDKITE_API_TOKEN=$(vault_with_retries read -field=token secret/ci/elastic-elasticsearch-serverless/buildkite-api-token)
+BUILD_JSON=$(curl -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" "https://api.buildkite.com/v2/organizations/elastic/pipelines/${INTAKE_PIPELINE_SLUG}/builds?branch=${KIBANA_BRANCH}&state=passed" | jq '. | map(. | select(.creator == null)) | .[0] | {commit: .commit, url: .web_url}')
+KIBANA_COMMIT=$(echo ${BUILD_JSON} | jq -r '.commit')
+
+echo "Last succesful kibana e2e test build: ${PROMOTED_BUILD_URL}" | buildkite-agent annotate --style "info" --context "e2e-test-base"
+echo "Kibana: : $KIBANA_COMMIT / $KIBANA_BRANCH" | buildkite-agent annotate --style "info" --context "kibana-version"
+
+echo "--- Trigger kibana e2e tests"
+cat <<EOF | buildkite-agent pipeline upload
+steps:
+    - label: ":pipeline: Trigger Kibana E2E with current ES Serverless"
+      trigger: kibana-elasticsearch-serverless-verify-and-promote
+      depends_on: docker-publish
+      build:
+        commit: "${KIBANA_COMMIT}"
+        branch: "${KIBANA_BRANCH}"
+        env:
+          ES_SERVERLESS_IMAGE: "${IMAGE_OVERRIDE}"
+          SKIP_CYPRESS: "1"
+          FTR_EXTRA_ARGS: "--include-tag=esGate"
+EOF
