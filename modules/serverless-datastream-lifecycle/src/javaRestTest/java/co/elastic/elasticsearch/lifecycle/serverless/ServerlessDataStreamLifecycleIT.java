d2503aac4174d4438880ecf0f8155967f4197d03
Andrei Dan <andrei.dan@elastic.co>
2023-11-15 15:29:17 +0000

    [DSL] Test the default lifecycle is configured (#1168)

--- modules/serverless-datastream-lifecycle/src/javaRestTest/java/co/elastic/elasticsearch/lifecycle/serverless/ServerlessDataStreamLifecycleIT.java
+++ modules/serverless-datastream-lifecycle/src/javaRestTest/java/co/elastic/elasticsearch/lifecycle/serverless/ServerlessDataStreamLifecycleIT.java
@@ -27,9 +27,12 @@ import org.elasticsearch.test.cluster.serverless.ServerlessElasticsearchCluster;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.junit.ClassRule;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 public class ServerlessDataStreamLifecycleIT extends ESRestTestCase {
@@ -52,30 +55,70 @@ public class ServerlessDataStreamLifecycleIT extends ESRestTestCase {
     }
 
     public void testILMPolicesAreNotInstalled() throws Exception {
-        try (var client = client()) {
-            ensureGreen("*");
+        ensureGreen("*");
 
-            // let's wait for a few component templates to be installed
-            assertBusy(() -> {
-                Response templatesResponse = client.performRequest(new Request("GET", "_component_template"));
-                String componentTemplatesResponse = EntityUtils.toString(templatesResponse.getEntity());
-                assertThat(componentTemplatesResponse, containsString("metrics-settings"));
-                assertThat(componentTemplatesResponse, containsString("logs-settings"));
-            }, 30, TimeUnit.SECONDS);
+        // let's wait for a few component templates to be installed
+        assertBusy(() -> {
+            Response templatesResponse = client().performRequest(new Request("GET", "_component_template"));
+            String componentTemplatesResponse = EntityUtils.toString(templatesResponse.getEntity());
+            assertThat(componentTemplatesResponse, containsString("metrics-settings"));
+            assertThat(componentTemplatesResponse, containsString("logs-settings"));
+        }, 30, TimeUnit.SECONDS);
 
-            // let's test no component template configures index.lifecycle.name
-            assertBusy(() -> {
-                Response templatesResponse = client.performRequest(new Request("GET", "_component_template"));
-                assertThat(EntityUtils.toString(templatesResponse.getEntity()), not(containsString("index.lifecycle.name")));
-            }, 30, TimeUnit.SECONDS);
+        // let's test no component template configures index.lifecycle.name
+        assertBusy(() -> {
+            Response templatesResponse = client().performRequest(new Request("GET", "_component_template"));
+            assertThat(EntityUtils.toString(templatesResponse.getEntity()), not(containsString("index.lifecycle.name")));
+        }, 30, TimeUnit.SECONDS);
 
-            // let's test no ILM policy exists in the cluster state
-            Response clusterStateResponse = client.performRequest(new Request("GET", "_cluster/state"));
-            assertThat(
-                "in serverless we run in data stream lifecycle only mode so no ILM policies should be installed",
-                EntityUtils.toString(clusterStateResponse.getEntity()),
-                not(containsString("index_lifecycle"))
+        // let's test no ILM policy exists in the cluster state
+        Response clusterStateResponse = client().performRequest(new Request("GET", "_cluster/state"));
+        assertThat(
+            "in serverless we run in data stream lifecycle only mode so no ILM policies should be installed",
+            EntityUtils.toString(clusterStateResponse.getEntity()),
+            not(containsString("index_lifecycle"))
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testDefaultLifecycleIsConfigured() throws Exception {
+        {
+            Request putComposableIndexTemplateRequest = new Request("POST", "/_index_template/template_without_lifecycle");
+            putComposableIndexTemplateRequest.setJsonEntity("{\"index_patterns\": [\"nolifecycle-*\"], \"data_stream\": {}}");
+            assertOK(client().performRequest(putComposableIndexTemplateRequest));
+
+            Request createDocRequest = new Request("POST", "/nolifecycle-myapp/_doc?refresh=true");
+            createDocRequest.setJsonEntity("{ \"@timestamp\": \"2022-12-12\"}");
+            assertOK(client().performRequest(createDocRequest));
+        }
+
+        {
+            Request putComposableIndexTemplateRequest = new Request("POST", "/_index_template/template_with_lifecycle");
+            putComposableIndexTemplateRequest.setJsonEntity(
+                "{\"index_patterns\": [\"withlifecycle-*\"], "
+                    + "\"data_stream\": {}, \"template\": { \"lifecycle\": { \"data_retention\": \"7d\" } } }"
             );
+            assertOK(client().performRequest(putComposableIndexTemplateRequest));
+
+            Request createDocRequest = new Request("POST", "/withlifecycle-myapp/_doc?refresh=true");
+            createDocRequest.setJsonEntity("{ \"@timestamp\": \"2022-12-12\"}");
+            assertOK(client().performRequest(createDocRequest));
+        }
+
+        Request getDataStreamsRequest = new Request("GET", "/_data_stream");
+        List<Map<String, Object>> getDataStreamsResponse = (List<Map<String, Object>>) entityAsMap(
+            client().performRequest(getDataStreamsRequest)
+        ).get("data_streams");
+        for (Map<String, Object> response : getDataStreamsResponse) {
+            if (response.get("name").equals("nolifecycle-myapp")) {
+                // we expect the default lifecycle to be configured for this data streams
+                assertThat(((Map<String, Object>) response.get("lifecycle")).get("enabled"), is(true));
+            } else if (response.get("name").equals("withlifecycle-myapp")) {
+                // the explicitly configured (7d retention) lifecycles must remain configured (i.e. the default lifecycle is only
+                // configured for data streams without an explicit lifecycle configuration)
+                assertThat(((Map<String, Object>) response.get("lifecycle")).get("data_retention"), is("7d"));
+                assertThat(((Map<String, Object>) response.get("lifecycle")).get("enabled"), is(true));
+            }
         }
     }
 }
