cd0c57a0934f080caad8025a26902ec8bd80cf90
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2024-04-26 10:27:57 +0300

    Revert "Revert "Add setting for disabling lossy params in source (#1679)" (#1…" (#1838)
    Roll forward with #1679
    
    https://github.com/elastic/kibana/pull/181742 should address Kibana test
    failures.

--- qa/stateful/x-pack/plugin/sql/qa/server/single-node/build.gradle.kts
+++ qa/stateful/x-pack/plugin/sql/qa/server/single-node/build.gradle.kts
@@ -30,5 +30,7 @@ tasks {
         exclude("**/RestSqlIT.class")
         // AwaitsFix: https://github.com/elastic/elasticsearch-serverless/issues/1611
         exclude("**/JdbcShardFailureIT.class")
+        // Lossy params in source mapper are not allowed in Serverless but included in these tests
+        exclude("**/FieldExtractorIT.class")
     }
 }
