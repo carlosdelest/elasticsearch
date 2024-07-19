7400d9be6b6c5845151e710db5042dfa801b632b
Tim Vernum <tim@adjective.org>
2024-07-19 13:47:57 +1000

    Re-enable trace logging on SSO IT (#2358)
    This test is failing again for unexplained reasons. Turning on TRACE
    logging for role mapping to try and determine why.
    
    Relates: #898

--- qa/sso-authentication/src/javaRestTest/java/co/elastic/elasticsearch/qa/sso/ServerlessSsoIT.java
+++ qa/sso-authentication/src/javaRestTest/java/co/elastic/elasticsearch/qa/sso/ServerlessSsoIT.java
@@ -113,6 +113,8 @@ public final class ServerlessSsoIT extends ESRestTestCase {
         .configFile("metadata.xml", Resource.fromClasspath("saml/metadata.xml"))
         .configFile("operator/settings.json", Resource.fromClasspath("operator/role-mappings.json"))
         .configFile("roles.yml", Resource.fromClasspath("roles.yml"))
+        // Logging enabled to debug https://github.com/elastic/elasticsearch-serverless/issues/898
+        .setting("logger.org.elasticsearch.xpack.security.authc.support.mapper", "TRACE")
         .build();
 
     private SamlFactory samlFactory;
