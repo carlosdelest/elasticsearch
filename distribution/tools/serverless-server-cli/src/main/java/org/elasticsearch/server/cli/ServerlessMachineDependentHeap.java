6c42251b8c935cb8208faa104695c2642fda1e1c
Matteo Piergiovanni <134913285+piergm@users.noreply.github.com>
2024-08-07 12:10:21 +0200

    Use half instead of a quarter system memory as heap for nodes with less than 8Gb (#2490)
    * Half system memory as heap for vector search nodes with less than 8GB memory
    
    * tests and lint
    
    * comments
    
    * iter

--- distribution/tools/serverless-server-cli/src/main/java/org/elasticsearch/server/cli/ServerlessMachineDependentHeap.java
+++ distribution/tools/serverless-server-cli/src/main/java/org/elasticsearch/server/cli/ServerlessMachineDependentHeap.java
@@ -30,18 +30,22 @@ import static java.lang.Math.min;
 public class ServerlessMachineDependentHeap extends MachineDependentHeap {
 
     // TODO: expose these from the super class
-    private static long GB = 1024L * 1024L * 1024L;
-    private static long MAX_HEAP = GB * 31;
+    private static final long GB = 1024L * 1024L * 1024L;
+    private static final long MAX_HEAP = GB * 31;
 
     @Override
     protected int getHeapSizeMb(Settings nodeSettings, MachineNodeRole role, long availableMemory) {
         ProjectType projectType = ServerlessSharedSettings.PROJECT_TYPE.get(nodeSettings);
+
         /*
          * Vector search data nodes require more file system cache.
-         * Heap is computed as 25% of available memory up to the normal max heap.
+         * Heap is computed as 50% for nodes with less than 8GB system memory.
+         * For nodes bigger or equal to 8Gb we use 25% of available memory up to the normal max heap.
          */
         if (projectType == ProjectType.ELASTICSEARCH_VECTOR && role == MachineNodeRole.DATA) {
-            return mb(min((long) (availableMemory * 0.25), MAX_HEAP));
+            long threshold = 8 * GB;
+            double factor = availableMemory < threshold ? 0.5 : 0.25;
+            return mb(min((long) (availableMemory * factor), MAX_HEAP));
         }
         return super.getHeapSizeMb(nodeSettings, role, availableMemory);
     }
--- distribution/tools/serverless-server-cli/src/test/java/org/elasticsearch/server/cli/ServerlessMachineDependentHeapTests.java
+++ distribution/tools/serverless-server-cli/src/test/java/org/elasticsearch/server/cli/ServerlessMachineDependentHeapTests.java
@@ -31,12 +31,35 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 
 @WithoutSecurityManager
 public class ServerlessMachineDependentHeapTests extends ESTestCase {
-    public void testVectorSearch() throws Exception {
+    private static SystemMemoryInfo systemMemoryInGigabytes(double gigabytes) {
+        return () -> (long) (gigabytes * 1024 * 1024 * 1024);
+    }
+
+    public void testVectorSearchMaxMemory() throws Exception {
+        // For bigger nodes we have a max heap of 31Gb == 31744Mb
+        assertHeapOptions(256, "31744m", ProjectType.ELASTICSEARCH_VECTOR, "data");
+    }
+
+    public void testVectorSearch8GbNode() throws Exception {
+        // For 8Gb nodes and up to max heap we have a 0.25 memory to heap ratio
         assertHeapOptions(8, "2048m", ProjectType.ELASTICSEARCH_VECTOR, "data");
     }
 
-    private static SystemMemoryInfo systemMemoryInGigabytes(double gigabytes) {
-        return () -> (long) (gigabytes * 1024 * 1024 * 1024);
+    public void testVectorSearchRandomMidSizeNode() throws Exception {
+        // Random tests for nodes >= 8Gb and heap smaller than max heap
+        int systemMemoryInGigabytes = 8 * randomIntBetween(1, 12);
+        int expectedHeap = (int) (0.25 * systemMemoryInGigabytes * 1024); // times 1024 because expected heap is in Mb
+        assertHeapOptions(systemMemoryInGigabytes, expectedHeap + "m", ProjectType.ELASTICSEARCH_VECTOR, "data");
+    }
+
+    public void testVectorSearch4GbNode() throws Exception {
+        // For nodes with less than 8Gb we have a 0.5 memory to heap ratio
+        assertHeapOptions(4, "2048m", ProjectType.ELASTICSEARCH_VECTOR, "data");
+    }
+
+    public void testVectorSearch2GbNode() throws Exception {
+        // For nodes with less than 8Gb we have a 0.5 memory to heap ratio
+        assertHeapOptions(2, "1024m", ProjectType.ELASTICSEARCH_VECTOR, "data");
     }
 
     private void assertHeapOptions(double memoryInGigabytes, String expectedMemory, ProjectType projectType, String... roles)
