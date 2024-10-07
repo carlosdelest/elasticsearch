ffb7f3312c65b2290096bfe549914ed47e8d5bd1
Iraklis Psaroudakis <kingherc@gmail.com>
2024-10-07 21:15:22 +0300

    Fast refresh indices should use RCO (#2830)
    * Fast refresh indices should use RCO
    
    Depends on core ES PR elastic/elasticsearch#113478 .
    
    Fast refresh indices should now use search shards, by using the new
    Refresh Cost Optimizations to refresh them. That means they can now
    start using search shards for gets/searches as well.
    
    The only difference now for fast refresh indices vs non fast refresh
    indices, is that the former have a lower min & default refresh interval
    (1s vs 5s) & are excluded from refresh throttling.
    
    Note that for BWC, we expect search shards to be upgraded first, before
    indexing shards. Until the cluster is fully upgraded, the promotable
    shards (whether upgraded or not) will still receive and execute
    gets/searches locally. An upgraded indexing node will refresh
    unpromotables (which must be upgraded search nodes).
    
    With the later ES-9563, we will re-add checks that indexing shards
    should not receive a get/search.
    
    Closes ES-9573
    Closes ES-9579
    
    * Disable flush throttler for the upgrade test
    
    * PR comments
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- qa/rolling-upgrade/src/javaRestTest/java/co/elastic/elasticsearch/qa/upgrade/ServerlessRollingUpgradeIT.java
+++ qa/rolling-upgrade/src/javaRestTest/java/co/elastic/elasticsearch/qa/upgrade/ServerlessRollingUpgradeIT.java
@@ -8,12 +8,17 @@
 
 package co.elastic.elasticsearch.qa.upgrade;
 
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Node;
 import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
 import org.elasticsearch.cluster.routing.UnassignedInfo;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.test.cluster.MutableSettingsProvider;
 import org.elasticsearch.test.cluster.serverless.ServerlessBwcVersion;
 import org.elasticsearch.test.cluster.serverless.ServerlessElasticsearchCluster;
@@ -24,7 +29,12 @@ import org.elasticsearch.xcontent.json.JsonXContent;
 import org.junit.ClassRule;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
+import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_ROUTING_EXCLUDE_GROUP_PREFIX;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ServerlessRollingUpgradeIT extends ESRestTestCase {
@@ -39,18 +49,25 @@ public class ServerlessRollingUpgradeIT extends ESRestTestCase {
         .setting("xpack.ml.enabled", "false")
         .setting("xpack.watcher.enabled", "false")
         .settings(NEW_CLUSTER_UPLOAD_MAX_COMMITS_SETTINGS_PROVIDER)
+        // TODO remove the settings overrides for indices.memory.interval and indices.disk.interval here and below (ES-9563)
+        .setting("indices.memory.interval", "1h")
+        .setting("indices.disk.interval", "-1")
         .user("admin-user", "x-pack-test-password")
         .withNode(
             indexNodeSpec -> indexNodeSpec.name("index-node-2")
                 .setting("node.roles", "[master,remote_cluster_client,ingest,index]")
                 .setting("xpack.searchable.snapshot.shared_cache.size", "16MB")
                 .setting("xpack.searchable.snapshot.shared_cache.region_size", "256KB")
+                .setting("indices.memory.interval", "1h")
+                .setting("indices.disk.interval", "-1")
         )
         .withNode(
             searchNodeSpec -> searchNodeSpec.name("search-node-2")
                 .setting("node.roles", "[remote_cluster_client,search]")
                 .setting("xpack.searchable.snapshot.shared_cache.size", "16MB")
                 .setting("xpack.searchable.snapshot.shared_cache.region_size", "256KB")
+                .setting("indices.memory.interval", "1h")
+                .setting("indices.disk.interval", "-1")
         )
         .build();
 
@@ -85,10 +102,162 @@ public class ServerlessRollingUpgradeIT extends ESRestTestCase {
         assertDocCount(client(), index2, docCount);
     }
 
+    // TODO remove this test (ES-9563)
+    public void testFastRefreshUpgrade() throws Exception {
+        record NodeDetails(String name, int indexInCluster, Node clientNode) {}
+        ;
+        final var indexNodes = new ArrayList<NodeDetails>(2);
+        final var searchNodes = new ArrayList<NodeDetails>(2);
+        Runnable updateNodes = () -> {
+            try {
+                closeClients();
+                initClient();
+                waitForNodes(4);
+                indexNodes.clear();
+                searchNodes.clear();
+                assertThat(cluster.getNumNodes(), equalTo(4));
+                for (int i = 0; i < cluster.getNumNodes(); i++) {
+                    String nodeName = cluster.getName(i);
+                    if (nodeName.contains("search")) {
+                        searchNodes.add(new NodeDetails(nodeName, i, client().getNodes().get(i)));
+                    } else {
+                        indexNodes.add(new NodeDetails(nodeName, i, client().getNodes().get(i)));
+                    }
+                }
+                logger.info("Updated nodes: indexNodes: {}, searchNodes: {}", indexNodes, searchNodes);
+            } catch (Exception e) {
+                throw new AssertionError(e);
+            }
+        };
+        updateNodes.run();
+
+        final String indexName = "idx-1";
+        createIndex(
+            indexName,
+            indexSettings(1, 1).put(UnassignedInfo.INDEX_DELAYED_NODE_LEFT_TIMEOUT_SETTING.getKey(), "0")
+                .put(IndexSettings.INDEX_REFRESH_INTERVAL_SETTING.getKey(), "1h")
+                // exclude the second index and search nodes, so we know the index starts on the first index and search nodes
+                .put(INDEX_ROUTING_EXCLUDE_GROUP_PREFIX + "._name", indexNodes.get(1).name + "," + searchNodes.get(1).name)
+                .build()
+        );
+        ensureGreen(indexName);
+
+        // Index documents
+        logger.info("Indexing two docs");
+        client().setNodes(indexNodes.stream().map(n -> n.clientNode).toList());
+        createDocument(indexName, "doc1", "f", "v1");
+        refresh(indexName);
+        createDocument(indexName, "doc2", "f", "v2");
+
+        // Checking the two documents
+        logger.info("Checking two docs");
+        client().setNodes(searchNodes.stream().map(n -> n.clientNode).toList());
+        assertDocCount(client(), indexName, 1);
+        assertThat(searchDocuments(indexName), equalTo(1));
+        assertThat(getDocuments(indexName, List.of("doc1", "doc2"), false), equalTo(1));
+        // The following causes a refresh and sets trackTranslogLocation to true
+        assertThat(getDocuments(indexName, List.of("doc1", "doc2"), true), equalTo(2));
+
+        // Index one more doc
+        logger.info("Index a third doc");
+        client().setNodes(indexNodes.stream().map(n -> n.clientNode).toList());
+        createDocument(indexName, "doc3", "f", "v3");
+
+        // From here on, we check the three documents, where doc3 is got by non-real-time mGETs only after the index is refreshed
+        Consumer<Boolean> checkDocuments = (doc3NonRealTimeVisible) -> {
+            try {
+                client().setNodes(searchNodes.stream().map(n -> n.clientNode).toList());
+                int expectedNonRealTimeDocs = doc3NonRealTimeVisible ? 3 : 2;
+                assertDocCount(client(), indexName, expectedNonRealTimeDocs);
+                assertThat(getDocuments(indexName, List.of("doc1", "doc2", "doc3"), false), equalTo(expectedNonRealTimeDocs));
+                assertThat(getDocuments(indexName, List.of("doc1", "doc2", "doc3"), true), equalTo(3));
+            } catch (Exception e) {
+                throw new AssertionError(e);
+            }
+        };
+
+        Consumer<Integer> upgradeNode = (indexInCluster) -> {
+            try {
+                cluster.upgradeNodeToVersion(indexInCluster, Version.CURRENT);
+                updateNodes.run();
+                ensureGreen(indexName);
+            } catch (Exception e) {
+                throw new AssertionError(e);
+            }
+        };
+
+        logger.info("Checking docs");
+        checkDocuments.accept(false);
+
+        // Upgrade first search node (that has the search shard)
+        upgradeNode.accept(searchNodes.get(0).indexInCluster);
+
+        logger.info("Checking docs after upgrading first search node");
+        checkDocuments.accept(false);
+
+        // Upgrade second search node (that does not have the search shard)
+        upgradeNode.accept(searchNodes.get(1).indexInCluster);
+
+        logger.info("Checking docs after upgrading second search node");
+        checkDocuments.accept(false);
+
+        // Upgrade first index node (that has the index shard and will flush)
+        upgradeNode.accept(indexNodes.get(0).indexInCluster);
+
+        logger.info("Checking docs after upgrading first index node");
+        checkDocuments.accept(true);
+
+        // Upgrade second index node, completing the cluster upgrade and thus searches happen on the search nodes
+        upgradeNode.accept(indexNodes.get(1).indexInCluster);
+
+        logger.info("Checking docs after upgrading second index node");
+        checkDocuments.accept(true);
+    }
+
     private void indexDocument(String index) throws IOException {
         Request indexRequest = new Request("POST", "/" + index + "/" + "_doc/");
         indexRequest.setJsonEntity(Strings.toString(JsonXContent.contentBuilder().startObject().field("f", "v").endObject()));
         assertOK(client().performRequest(indexRequest));
+    }
+
+    private void createDocument(String index, String id, String fieldName, String fieldValue) throws IOException {
+        Request indexRequest = new Request("POST", "/" + index + "/" + "_create/" + id);
+        indexRequest.setJsonEntity(Strings.toString(JsonXContent.contentBuilder().startObject().field(fieldName, fieldValue).endObject()));
+        assertOK(client().performRequest(indexRequest));
+    }
+
+    private int getDocuments(String index, List<String> ids, boolean realtime) throws IOException {
+        Request request = new Request("GET", "/" + index + "/" + "_mget?realtime=" + realtime);
+        var json = JsonXContent.contentBuilder().startObject().startArray("docs");
+        for (String id : ids) {
+            json.startObject().field("_id", id).endObject();
+        }
+        json.endArray().endObject();
+        String jsonString = Strings.toString(json);
+        request.setJsonEntity(jsonString);
+        Response response = client().performRequest(request);
+        assertOK(response);
+        String responseString = EntityUtils.toString(response.getEntity());
+        var jsonResponse = XContentHelper.convertToMap(JsonXContent.jsonXContent, responseString, false);
+        var docs = (List) jsonResponse.get("docs");
+        var foundDocs = 0;
+        for (Object doc : docs) {
+            var docMap = (Map) doc;
+            if ((Boolean) docMap.get("found")) {
+                foundDocs++;
+            }
+        }
+        return foundDocs;
+    }
+
+    private Integer searchDocuments(String index) throws IOException {
+        Request request = new Request("GET", "/" + index + "/" + "_search");
+        Response response = client().performRequest(request);
+        assertOK(response);
+        var jsonResponse = XContentHelper.convertToMap(JsonXContent.jsonXContent, EntityUtils.toString(response.getEntity()), false);
+        var hits = (Map) jsonResponse.get("hits");
+        var hitsTotal = (Map) hits.get("total");
+        return Integer.parseInt(hitsTotal.get("value").toString());
     }
 
     private void performUpgrade() throws IOException {
