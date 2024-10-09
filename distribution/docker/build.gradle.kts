d7eb3bb1915eac5a8f45b3e9180eb19330d4bf5d
Rene Groeschke <rene@elastic.co>
2024-10-09 17:15:10 +0200

    Revert "Make serverless docker image base on elasticsearch wolfi image (#2889)" (#2940)
    This reverts commit 65d27cff77d6c0556fc4cb32a048847c58f0f19d.
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- distribution/docker/build.gradle.kts
+++ distribution/docker/build.gradle.kts
@@ -52,7 +52,7 @@ val dockerBuildTasks = Architecture.values().associateWith { architecture ->
     val upstreamContext = configurations.detachedConfiguration(dependencies.create("org.elasticsearch:docker")).apply {
         attributes {
             attribute(Attribute.of(Architecture::class.java), architecture)
-            attribute(Attribute.of(DockerBase::class.java), DockerBase.WOLFI)
+            attribute(Attribute.of(DockerBase::class.java), DockerBase.DEFAULT)
         }
     }
 
