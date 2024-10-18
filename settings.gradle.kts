d87430cb1e640df760a85f87465640950686631d
Mark Vieira <portugee@gmail.com>
2024-10-18 14:10:51 -0700

    Update develocity plugin (#3002)
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version ("3.17.4")
+    id("com.gradle.develocity") version ("3.18.1")
     id("elasticsearch.java-toolchain")
 }
 
