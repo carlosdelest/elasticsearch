a23c99362fef35cf49e94d5301bca57b24d0e8f0
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2024-10-21 09:15:18 -0700

    Exclude test that uses snapshot creation API (#3003)

--- qa/stateful/modules/data-streams/build.gradle.kts
+++ qa/stateful/modules/data-streams/build.gradle.kts
@@ -9,11 +9,10 @@ tasks {
     }
     javaRestTest {
         exclude("**/DataStreamDeleteLifecycleWithPermissionsRestIT.class")
-        exclude("**/StandardVersusLogsIndexModeRandomDataChallengeRestIT.class")
-        exclude("**/StandardVersusStandardReindexedIntoLogsDbChallengeRestIT.class")
-        exclude("**/LogsDbVersusReindexedLogsDbChallengeRestIT.class")
-        exclude("**/LogsDbVersusLogsDbReindexedIntoStandardModeChallengeRestIT.class")
-        // Lossy params in source mapper are not allowed in Serverless but included in these tests
-        exclude("**/LogsIndexModeCustomSettingsIT.class")
+
+        filter {
+            // Uses snapshot API not available as user API on serverless
+            excludeTestsMatching("*.LogsDataStreamRestIT.testLogsDBSnapshotCreateRestoreMount")
+        }
     }
 }
