f3b408fbb79ec7775c31f5dc98bf3115ef052191
Nhat Nguyen <nhat.nguyen@elastic.co>
2024-10-29 07:42:45 -0700

    Do not override number shards of lookup indices in stateless (#3038)
    We already have an index setting provider in the stateful to inject the 
    number of shards for lookup indices, so the stateless plugin should not
    override this setting.
    
    Relates elastic/elasticsearch#115143

--- qa/core-rest-test/build.gradle.kts
+++ qa/core-rest-test/build.gradle.kts
@@ -219,10 +219,7 @@ tasks {
 
                 //Tests rely on updating replicas which is not supported in Serverless
                 "indices.put_settings/10_basic/*",
-                "indices.put_settings/20_update_non_dynamic_settings/*",
-
-                // Muted until the stateless plugin no longer overrides number_of_shards of lookup indices
-                "indices.create/10_basic/Create lookup index*"
+                "indices.put_settings/20_update_non_dynamic_settings/*"
 
             ).joinToString(",")
         )
