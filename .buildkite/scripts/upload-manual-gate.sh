7058414351bddd65a28c5b97b7e8b102323d9165
Rene Groeschke <rene@elastic.co>
2024-12-12 22:26:03 +0100

    [Releasing] Detect quality gate reruns and skip baking period in rerun (#3256)
    This make handling quality gate general error handling easier as we
    can just resubmit the quality gate argo workflow to avoid manual tinkering
    with promotions.
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- .buildkite/scripts/upload-manual-gate.sh
+++ .buildkite/scripts/upload-manual-gate.sh
@@ -7,18 +7,24 @@ dependentSteps=($(buildkite-agent step get "depends_on" --format json | jq -r '.
 # Iterate over the list of dependent steps and check if any of them failed
 for step in "${dependentSteps[@]}"; do
   echo "Checking step $step"
-  if [[ "$(buildkite-agent step get "outcome" --step "${step}")" != "passed" ]]; then
-    echo "One or more quality gate steps ($step) failed. Adding manual quality gate override."
-      cat <<- YAML | buildkite-agent pipeline upload
-    - group: ":judge: Manual Verification"
-      steps:
-        - label: ":pipeline: Upload manual step"
-          command: "make -C /agent trigger-manual-verification-phase"
-          agents:
-            image: "docker.elastic.co/ci-agent-images/manual-verification-agent:0.0.4"
+  outcome="$(buildkite-agent step get "outcome" --step "${step}")"
+  echo "outcome '$outcome'"
+  if [[ -z "$outcome" ]]; then
+    echo "Skipping empty or invalid step"
+    continue
+  fi
+  if [[ "$outcome" != "passed" ]] && [[ "$outcome" != "broken" ]]; then
+      echo "One or more quality gate steps ($step) failed ($outcome). Adding manual quality gate override."
+       cat <<- YAML | buildkite-agent pipeline upload
+     - group: ":judge: Manual Verification"
+       steps:
+         - label: ":pipeline: Upload manual step"
+           command: "make -C /agent trigger-manual-verification-phase"
+           agents:
+             image: "docker.elastic.co/ci-agent-images/manual-verification-agent:0.0.4"
 YAML
   break
   else
-      echo "Step $step passed. Continuing..."
+      echo "Step $step passed or not part of the pipeline. Continuing..."
   fi
 done
