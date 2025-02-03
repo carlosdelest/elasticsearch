d0f4d50668aaa7d82a4ea61c2745fb638f03561d
Luca Cavanna <javanna@apache.org>
2025-02-03 10:27:32 +0100

    Disable graph tests in Serverless (#3410)
    * Disable graph tests in Serverless
    
    * Merge upstream and update elasticsearch submodule to 487b217afe200028d1f27a25546e29cf8160983f
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- qa/x-pack-rest-test/build.gradle.kts
+++ qa/x-pack-rest-test/build.gradle.kts
@@ -111,7 +111,10 @@ tasks {
             // Global privileges not supported in Serverless
             "roles/40_global_privileges/*",
             // The migrate functionality will not be used in serverless yet
-            "migrate/*/*"
+            "migrate/*/*",
+            // Graph API is not available in Serverless
+            "graph/*/*"
+
         ).joinToString(","))
     }
 }
