fe07907ad3a96df7554d39ee48ffce299742faaf
Pooya Salehi <pxsalehi@users.noreply.github.com>
2025-04-24 11:49:23 +0200

    Consider merge memory usage estimate in indexing tier metrics (#3666)
    * draft wip
    
    * more draft
    
    * change
    
    * draft
    
    * update
    
    * temp
    
    * cleanup
    
    * settings and stuff
    
    * cleanup
    
    * export setting
    
    * change
    
    * more corner cases
    
    * more tests
    
    * republish on master failure
    
    * change
    
    * cleanup
    
    * Address Henning's feedback
    
    * Address Nick's feedback
    
    * Remove handling of out-of-order merge events
    
    * update submodule
    
    * update TV
    
    * update submodule
    
    * Update modules/stateless/src/main/java/co/elastic/elasticsearch/stateless/autoscaling/memory/MemoryMetricsService.java
    
    Co-authored-by: Henning Andersen <33268011+henningandersen@users.noreply.github.com>
    
    * update TV and use mutex
    
    * Merge upstream and update elasticsearch submodule to f599fe32decb67ca410f4d7e24ea5b36dd91ca51
    
    ---------
    
    Co-authored-by: Henning Andersen <33268011+henningandersen@users.noreply.github.com>
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- libs/serverless-shared-constants/src/main/java/co/elastic/elasticsearch/serverless/constants/ServerlessTransportVersions.java
+++ libs/serverless-shared-constants/src/main/java/co/elastic/elasticsearch/serverless/constants/ServerlessTransportVersions.java
@@ -34,6 +34,7 @@ public class ServerlessTransportVersions {
 
     // public static final TransportVersion EXAMPLE_SERVERLESS_VERSION = def(9_000_1_00);
     public static final TransportVersion INDEXING_OPERATIONS_MEMORY_REQUIREMENTS = def(9_050_1_00);
+    public static final TransportVersion AUTOSCALING_MERGE_MEMORY_ESTIMATE_SERVERLESS_VERSION = def(9_064_1_00);
 
     /*
      * STOP! READ THIS FIRST! No, really,
