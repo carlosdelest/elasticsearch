8122726f51ea4d50a6b4e823b6c40e89b8d92dd4
Jordan Powers <jordan.powers@elastic.co>
2025-07-10 14:45:45 -0700

    Fix transport version for TRACK_POSTINGS_IN_MEMORY_BYTES (#4194)
    The serverless transport version is supposed to follow the latest
    non-serverless version at merge time, but I forgot to update it before
    merging #4143. So I need this follow-up to fix it.

--- libs/serverless-shared-constants/src/main/java/co/elastic/elasticsearch/serverless/constants/ServerlessTransportVersions.java
+++ libs/serverless-shared-constants/src/main/java/co/elastic/elasticsearch/serverless/constants/ServerlessTransportVersions.java
@@ -36,7 +36,7 @@ public class ServerlessTransportVersions {
     public static final TransportVersion INDEXING_OPERATIONS_MEMORY_REQUIREMENTS = def(9_050_1_00);
     public static final TransportVersion AUTOSCALING_MERGE_MEMORY_ESTIMATE_SERVERLESS_VERSION = def(9_064_1_00);
     public static final TransportVersion METERING_CLUSTER_STATE_METADATA_INDEX_UUID_FLAG_CLEANUP = def(9_074_1_00);
-    public static final TransportVersion TRACK_POSTINGS_IN_MEMORY_BYTES = def(9_118_1_00);
+    public static final TransportVersion TRACK_POSTINGS_IN_MEMORY_BYTES = def(9_121_1_00);
 
     /*
      * STOP! READ THIS FIRST! No, really,
